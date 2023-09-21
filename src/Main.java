db9d511e0021de27fd04fefafb71dd646289c7ed
KT <thun.kristian@gmail.com>
2023-09-21 15:04:40 +0200

    Base game is done

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,90 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Die die1 = new Die(6);
-        for (int i =0; i <= 10; i++) {
-            die1.roll();
-            System.out.println(die1);
+        int numberOfRounds = 1;
+
+        ArrayList<Player> players = initialize();
+
+        for (int i = 0; i < numberOfRounds; i++){
+            System.out.println();
+            takeTurn(players);
         }
 
+        ArrayList<Player> winners =  getWinners(players);
 
+        StringBuilder builder = new StringBuilder();
+        for (Player player: winners) {
+            builder.append(player.getName());
+            builder.append(", ");
+        }
 
+        String nameOfWinners = builder.toString();
+
+        System.out.println((winners.size() > 1 ?  "The Winners are ": "The Winner is ") + nameOfWinners +
+                "with " + winners.get(0).getScore() + " point " + (winners.get(0).getScore() == 1 ? "" : "s"));
+
+    }
+
+    public static ArrayList<Player> initialize(){
+        ArrayList<Player> players = new ArrayList<>();
+
+        System.out.println("How many players?");
+        int numberOfPlayers = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("How many dice?");
+        int numberOfDice = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("How many sides on the dice?");
+        int numberOfSides = sc.nextInt();
+        sc.nextLine();
+
+        for(int i = 1; i <= numberOfPlayers; i++){
+            System.out.println("Name for player " + i);
+            String name = sc.nextLine();
+
+            Player player = new Player(name);
+            for (int j = 0; j < numberOfDice; j++){
+                player.addDie(numberOfSides);
+            }
+            players.add(player);
+        }
+
+        return players;
+    }
+
+    public static void takeTurn(ArrayList<Player> players){
+        for (Player player: players) {
+            System.out.println(player.getName() + " which number will you roll?");
+            int guess = sc.nextInt();
+            sc.nextLine();
+
+            player.rollDice();
+            if (guess == player.getDieValue()){
+                player.increaseScore();
+                System.out.println( player.getName() + " rolled a " + player.getDieValue() + ". 1 point for you!");
+            }
+            else {
+                System.out.println(player.getName() + " rolled a " + player.getDieValue());
+            }
+            System.out.println();
+        }
+    }
+
+    public static ArrayList<Player> getWinners(ArrayList<Player> players){
+        ArrayList<Player> winners = new ArrayList<>();
+        for(Player player: players){
+            if(winners.isEmpty() || player.getScore() == winners.get(0).getScore()){
+                winners.add(player);
+            } else if (player.getScore() > winners.get(0).getScore()) {
+                winners.clear();
+                winners.add(player);
+            }
+        }
+        return winners;
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Player {
+    private String name;
+    private int score;
+
+    ArrayList<Die> dice;
+
+    public Player(String name) {
+        this.name = name;
+        this.dice = new ArrayList<>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void rollDice() {
+        for (Die die : dice) {
+            die.roll();
+        }
+    }
+
+    public int getDieValue() {
+        int sum = 0;
+        for (Die die : dice) {
+            sum += die.getCurrentValue();
+        }
+        return sum;
+    }
+
+    public void increaseScore() {
+        score++;
+    }
+
+    public void addDie(int sides){
+        Die newDie = new Die(sides);
+        dice.add(newDie);
+    }
+
+    public String toString(){
+
+        return "Name: " + name + " Score: " + score + " Number of Dice:  " + dice.size() + " " + dice;
+
+    }
+
+}
+
